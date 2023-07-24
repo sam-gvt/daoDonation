6a5ad471709ce204af7ff9c450aee06da954091e
sam-gvt <compteprosam@gmail.com>
2023-07-24 16:17:59 +0200

    goerli

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RPC_URL = process.env.RPC_URL;
+const PK_addr1 = process.env.PK_addr1;
+// const PK_addr2 = process.env.PK_addr2;
+// const PK_addr3 = process.env.PK_addr3;
+
+//, `0x${PK_addr2}`, `0x${PK_addr3}`
+
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+    goerli: {
+      url: `${RPC_URL}`,
+      accounts: [`0x${PK_addr1}`],
+    },
+    // localhost: {
+    //   url: "http://127.0.0.1:8545"
+    // },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18"
+      }
+    ]
+  },
 };
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -12,19 +12,26 @@ import { WagmiConfig, createConfig } from 'wagmi'
 import { hardhat, goerli } from 'wagmi/chains';
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+
 
 
 import { configureChains } from '@wagmi/core'
  
+const API_KEY_INFURA = process.env.API_KEY_INFURA;
 
 const { chains, publicClient } = configureChains(
-  [hardhat, goerli],
+  [goerli],
   [
-    jsonRpcProvider({
-      rpc: () => ({
-        http: "http://127.0.0.1:8545"
-      }),
-    })
+    // jsonRpcProvider({
+    //   rpc: () => ({
+    //     http: "http://127.0.0.1:8545"
+    //   }),
+    // }),
+    //publicProvider(),
+    infuraProvider({ apiKey: API_KEY_INFURA })
+    
   ]
 );
 const config = createConfig({
