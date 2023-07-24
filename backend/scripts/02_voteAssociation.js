8cd8620f7cace1322cba42c57cf0f705806a541e
sam-gvt <compteprosam@gmail.com>
2023-07-24 14:28:13 +0200

    readme

--- backend/scripts/02_voteAssociation.js
+++ backend/scripts/02_voteAssociation.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat")
 async function main() {
 
     [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
-    let admins = [owner, addr1, addr2];
+    let admins = [owner, addr1];
 
 
     const _hardhatContract = await hre.ethers.getContractAt("Donation", "0x5FbDB2315678afecb367f032d93F642f64180aa3")
