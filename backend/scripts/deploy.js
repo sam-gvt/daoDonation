2913277c82c1040ea0b37c7cf679e62ceef9c065
sam-gvt <compteprosam@gmail.com>
2023-07-25 14:30:01 +0200

    commit

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,11 +1,11 @@
 async function main() {
-  //const [owner, addr1, addr2] = await ethers.getSigners();
-  const [deployer] = await ethers.getSigners();
+  const [owner, addr1, addr2] = await ethers.getSigners();
+  //const [deployer] = await ethers.getSigners();
   //console.log("Deploying contracts with the admins :", owner.address, addr1.address);
-  console.log("Deploying contracts with the account:", deployer.address);
+  //console.log("Deploying contracts with the account:", deployer.address);
 
-  //let admins = [owner, addr1, addr2];
-  let admins = [deployer];
+  let admins = [owner, addr1, addr2];
+  //let admins = [deployer];
   //console.log(admins);
   const donation = await ethers.deployContract("Donation", [admins]);
 
